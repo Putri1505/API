92770f696cc364d1f84f6320f1ca313e0146b035
Putri Ayu Pratiwi <utimarutiii@gmail.com>
2021-05-28 09:29:54 +0700

    Controllers
    Menambah Controller

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using API.Base;
+using API.Models;
+using API.Repository.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseController<Account, AccountRepository, int>
+    {
+        public AccountController(AccountRepository account) : base(account)
+        {
+        }
+    }
+}
---
+++ API/Controllers/ProfilingController.cs
@@ -0,0 +1,18 @@
+﻿using API.Base;
+using API.Models;
+using API.Repository.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class ProfilingController : BaseController<Profiling, ProfilingRepository, int>
+    {
+        public ProfilingController(ProfilingRepository profiling) : base(profiling)
+        {
+
+        }
+    }
+}
